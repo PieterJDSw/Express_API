730462a43499d89c899097e5bce8df9bb2777814
PieterSwanepoelEntelect <pieter.swanepoel@entelect.co.za>
2023-07-05 19:32:24 +0200

    bug fix

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,9 +2,15 @@ const User = require("../User")
 const express = require('express')
 const router = express.Router()
 
-// middleware that is specific to this router
+// middleware that is specific to this router or add globally to be added to all routes
+// Good example for this would be JWT authentication
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
+  res.set({
+    'Server': "express",
+    'Demo':"Entelect Tech talk"
+    
+  });
   next()
 })
 // Get all users
@@ -14,12 +20,12 @@ router.get('/', async (req, res) => {
 })
 // Get by ID
 router.get('/:userId', async (req, res) => {
-    let userId = request.params.userId
+    let userId = req.params.userId
     const user = await User.findById(userId)
     res.send(user)
 })
 router.post('/', async (req, res) => {
-    let user = request.body
+    let user = req.body
     User.create(user)
     res.send("User Created")
 })
